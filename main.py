821f20656b43142f6a36a8d5adca5b250828fa19
akshansh <akshanshgoyal020@gmail.com>
2026-05-24 23:41:44 +0530

    changed api structure

--- main.py
+++ main.py
@@ -32,47 +32,51 @@ projects = ["Akshansh", "Akshansh-api"]
 projects_dict = {"Akshansh":1, "Akshansh-api":2}
 blogs = ["lorem", "ipsum"]
 blogs_dict = {"lorem":1,"ipsum":2}
+writeups = ["lorem", "ipsum"]
+writeups_dict = {"lorem":1, "ipsum":2}
 
-@app.get("/projects")
+@app.get()
 @ttl_cache()
-def get_projects():
-    responses = {}
-    for project in projects:
-        req = requests.get(f"https://raw.githubusercontent.com/Ender-always-wins/{project}/main/README.md")
-        responses[project] = req.text.replace("\n","").replace("    "," ")
-    return responses
-
-@app.get("/projects/{project}")
-@ttl_cache()
-def get_project(project: str):
-    if project not in projects:
-        return fastapi.Response(status_code=404)
-    req = requests.get(f"https://raw.githubusercontent.com/Ender-always-wins/{project}/main/README.md")
-    return req.text.replace("\n","").replace("    "," ")
-
-@app.get("/names")
-@ttl_cache()
-def get_names():
-    return projects_dict
-
-@app.get("/blogs")
-@ttl_cache()
-def get_blogs():
-    responses = {}
-    for blog in blogs:
-        req = requests.get(f"https://raw.githubusercontent.com/Ender-always-wins/blogs/main/{blog}.html")
-        responses[blog] = req.text.replace("\n","").replace("    "," ")
-    return responses
-
-@app.get("/blogs/{blog}")
-@ttl_cache()
-def get_blog(blog: str):
-    if blog not in blogs:
-        return fastapi.Response(status_code=404)
-    req = requests.get(f"https://raw.githubusercontent.com/Ender-always-wins/blogs/main/{blog}.html")
-    return req.text.replace("\n","").replace("    "," ")
-
-@app.get("/blog-names")
-@ttl_cache()
-def get_blog_names():
-    return blogs_dict
+def get_all(
+    type: str,
+    name: str | None = None,
+    names: bool = False
+):
+    if names:
+        if type == "projects":
+            return projects_dict
+        if type == "blogs":
+            return blogs_dict
+        if type == "writeups":
+            return writeups_dict
+    else:
+        if type == "projects":
+            if name == None:
+                responses = {}
+                for project in projects:
+                    req = requests.get(f"https://raw.githubusercontent.com/Ender-always-wins/{project}/main/README.md")
+                    responses[project] = req.text.replace("\n","").replace("    "," ")
+                return responses
+            else:
+                req = requests.get(f"https://raw.githubusercontent.com/Ender-always-wins/{name}/main/README.md")
+                return req.text.replace("\n","").replace("    "," ")
+        if type == "blogs":
+            if name == None:
+                responses = {}
+                for blog in blogs:
+                    req = requests.get(f"https://raw.githubusercontent.com/Ender-always-wins/blogs/main/{blog}.html")
+                    responses[blog] = req.text.replace("\n","").replace("    "," ")
+                return responses
+            else:
+                req = requests.get(f"https://raw.githubusercontent.com/Ender-always-wins/blogs/main/{name}.html")
+                return req.text.replace("\n","").replace("    "," ")
+        if type == "writeups":
+            if name == None:
+                responses = {}
+                for writeup in writeups:
+                    req = requests.get(f"https://raw.githubusercontent.com/Ender-always-wins/blogs/main/writeups/{writeup}.html")
+                    responses[writeup] = req.text.replace("\n","").replace("    "," ")
+                return responses
+            else:
+                req = requests.get(f"https://raw.githubusercontent.com/Ender-always-wins/blogs/main/{name}.html")
+                return req.text.replace("\n","").replace("    "," ")
